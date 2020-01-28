473061d04d5f6bddec08ca9db6f1268141d6afca
kasp4770 <kasp4770@edu.easj.dk>
2020-01-28 14:05:48 +0100

    Seeder file added

--- db/migrations/20200127095441-create-address.js
+++ db/migrations/20200127095441-create-address.js
@@ -19,11 +19,13 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: '1999-10-10'
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: '1999-10-10'
       }
     });
   },
